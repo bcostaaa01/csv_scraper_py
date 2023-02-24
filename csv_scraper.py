c41f9ee92d9723724310bf78b1f386c0629ff7bc
Timur Amirov <timur.amirov@actio.com>
2023-02-24 17:11:43 +0100

    Make input and output filenames configurable
    First we extract main source-code into a separate function (main),
    then pass default params from within the script

--- csv_scraper.py
+++ csv_scraper.py
@@ -1,15 +1,24 @@
 import csv
 
-# open the CSV file
-with open('people-100.csv') as input_file:
-    # create a CSV reader object
-    csv_reader = csv.reader(input_file)
+def main(params):
+    input_filename, output_filename = params["input"], params["output"]
 
-    with open('newFile.csv', mode='w', newline='') as output_file:
-        csv_writer = csv.writer(output_file)
-        # iterate over each row in the CSV file
-        for row in csv_reader:
-            # write new CSV file with value of row[2]
-            value = row[2]
+    # open the CSV file
+    with open(input_filename) as input_file:
+        # create a CSV reader object
+        csv_reader = csv.reader(input_file)
 
-            csv_writer.writerow([value])
+        with open(output_filename, mode='w', newline='') as output_file:
+            csv_writer = csv.writer(output_file)
+            # iterate over each row in the CSV file
+            for row in csv_reader:
+                # write new CSV file with value of row[2]
+                value = row[2]
+
+                csv_writer.writerow([value])
+
+params = {
+    "input": "people-100.csv",
+    "output": "newFile.csv",
+}
+main(params)
