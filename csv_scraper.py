e3d359755aca920ab0feb5f713ddd062fee30d22
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2023-02-24 11:32:53 +0000

    Add functionality to create a new CSV file
    - Use `csv.writer()` to write on newFile.csv file
    - Use value of row[2] to write row on newFile.csv

--- csv_scraper.py
+++ csv_scraper.py
@@ -1,9 +1,15 @@
 import csv
 
-with open('filename.csv') as file:
+# open the CSV file
+with open('people-100.csv') as input_file:
     # create a CSV reader object
-    csv_reader = csv.reader(file)
+    csv_reader = csv.reader(input_file)
 
-    # iterate over each row in the CSV file
-    for row in csv_reader:
-        print(row)
+    with open('newFile.csv', mode='w', newline='') as output_file:
+        csv_writer = csv.writer(output_file)
+        # iterate over each row in the CSV file
+        for row in csv_reader:
+            # write new CSV file with value of row[2]
+            value = row[2]
+
+            csv_writer.writerow([value])
